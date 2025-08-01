673d70edf1a99d11a30d5d26e106eea9f48855c7
Grandench1k <salams2371@gmail.com>
2025-08-01 21:43:53 +0300

    init commit

---
+++ src/main/java/grandench1k/springcorehomework/SpringCoreHomeworkApplication.java
@@ -0,0 +1,13 @@
+package grandench1k.springcorehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreHomeworkApplication.class, args);
+    }
+
+}
